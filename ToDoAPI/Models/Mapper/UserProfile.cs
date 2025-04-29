4fadc29a2f269ef0c59915e57fb5cbd74e6513c8
Gaytannn <agaytanjesus@gmail.com>
2025-04-29 17:35:17 -0600

    JG 20252904 add Mapper User

---
+++ ToDoAPI/Models/Mapper/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using ToDoAPI.Models.Request;
+
+namespace ToDoAPI.Models.Mapper;
+
+public static class UserProfile
+{
+    public static User ToUser(this UserRequest request)
+    {
+        return new()
+        {
+            Username = request.User,
+            PasswordHash = request.Password
+        };
+    }
+}
