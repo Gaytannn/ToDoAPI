24760ab38661a0d2ca175d04f3629e90fc734acc
Gaytannn <agaytanjesus@gmail.com>
2025-04-29 17:34:30 -0600

    JG 20252904 add Jwt Token

---
+++ ToDoAPI/Sevices/JwtTokenService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ToDoAPI.Interfaces;
+
+namespace ToDoAPI.Sevices;
+
+public class JwtTokenService:IJwtTokenService
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtTokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string GenerateToken(string username, Guid id)
+    {
+        var jwtSettings = _configuration.GetSection("JwtSettings");
+        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]));
+        var credentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier,id.ToString()),
+            new Claim(ClaimTypes.Name, username)
+
+        };
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["ExpirationInMinutes"])),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    public bool ValidateToken(string token, out string username)
+    {
+        username = null;
+        var jwtSettings = _configuration.GetSection("JwtSettings");
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var secretKey = Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]);
+
+        try
+        {
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(secretKey)
+            };
+
+            var principal = tokenHandler.ValidateToken(token, validationParameters, out var validatedToken);
+            username = principal.Identity.Name;
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
